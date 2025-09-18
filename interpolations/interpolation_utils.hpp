a3f50154b402ac52c381f011561ae7e357d41218
Keo Ponleou Sok <ponleousk@gmail.com>
2025-09-18 18:18:24 +1000

    docs: used headers for separation

---
+++ interpolations/interpolation_utils.hpp
@@ -0,0 +1,75 @@
+#pragma once
+extern "C"
+{
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+#include <libavutil/pixdesc.h>
+#include <libswscale/swscale.h>
+}
+#include <array>
+#include <hip/amd_detail/amd_hip_runtime.h>
+#include <hip/hip_runtime.h>
+#include <hip/hip_runtime_api.h>
+#include <iostream>
+using std::array;
+using std::cerr;
+using std::copy;
+using std::invalid_argument;
+using std::to_string;
+
+// just shutting up intellisense
+#ifdef __INTELLISENSE__
+#define threadIdx (dim3{})
+#define blockIdx (dim3{})
+#define blockDim (dim3{})
+#endif
+
+#define HIP_CHECK(expression)                 \
+    {                                         \
+        const hipError_t status = expression; \
+        if (status != hipSuccess)             \
+        {                                     \
+            cerr << "HIP error "              \
+                 << status << ": "            \
+                 << hipGetErrorString(status) \
+                 << " at " << __FILE__ << ":" \
+                 << __LINE__ << std::endl;    \
+        }                                     \
+    }
+
+template <typename T, int K>
+class GPUMemory
+{
+private:
+    array<T *, K> memory;
+    array<int, K> sizes;
+
+public:
+    GPUMemory(const int (&sizes)[K])
+    {
+        copy(sizes, sizes + K, this->sizes.begin());
+        for (int i = 0; i < K; i++)
+            HIP_CHECK(hipMalloc((void **)&this->memory[i], this->sizes[i] * sizeof(T)));
+    }
+
+    ~GPUMemory()
+    {
+        for (int i = 0; i < K; i++)
+        {
+            HIP_CHECK(hipFree(this->memory[i]));
+        }
+    }
+
+    T *&operator[](int index)
+    {
+        return this->memory[index];
+    }
+
+    // allows function to check that memory sizes are as expected before passing to kernel
+    void validateMemorySizes(const int (&sizes)[K])
+    {
+        for (int i = 0; i < K; i++)
+            if (sizes[i] != this->sizes[i])
+                throw invalid_argument("Memory sizes don't match at index " + to_string(i));
+    }
+};
